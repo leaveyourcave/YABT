8b2f7e967e17756684c9fe3dae5eaae842989c0e
leaveyourcave <leaveyourcave@gmail.com>
2017-03-04 21:37:50 +0100

    Creating chart object with static data. Now, chart appears on Dashboard page.

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -14,5 +14,33 @@ export class DashboardPage {
 
     ionViewDidLoad() {
         console.log('ionViewDidLoad DashboardPage');
+
+        let dashboardChartArea = this.dashboardChart.nativeElement;
+
+        c3.generate({
+            bindto: dashboardChartArea,
+            data: {
+                type: 'donut',
+                columns: [
+                    ['Under 10', 30],
+                    ['10-12', 25],
+                    ['13-15', 20],
+                    ['15-18', 15],
+                    ['Above 18', 10]
+                ],
+                onclick: function(d, i) {
+                    console.log("Button clicked!", d, i);
+                },
+                onmouseover: function(d, i) {
+                    console.log("Mouse over!", d, i);
+                },
+                onmouseout: function(d, i) {
+                    console.log("Mouse out!", d, i);
+                }
+            },
+            donut: {
+                title: "Users age"
+            }
+        });
     }
 }
