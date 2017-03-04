18381abd4f3b8a00c98cb0d461770df16a32fe34
leaveyourcave <leaveyourcave@gmail.com>
2017-03-04 21:28:05 +0100

    Adding C3 chart to html and getting reference to that chart in DashboardPage component

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import * as c3 from 'c3';
 
 @Component({
     selector: 'page-dashboard',
     templateUrl: 'dashboard.html'
 })
 export class DashboardPage {
+
+    @ViewChild('dashboardChart') dashboardChart: ElementRef;
 
     constructor(public navCtrl: NavController, public navParams: NavParams) { }
 
