b110257832956cc863826e7543562611d549abdd
leaveyourcave <leaveyourcave@gmail.com>
2017-03-07 20:27:39 +0100

    Custom tooltip has been added. Dirty version.

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Database } from "../../providers/database";
 import * as c3 from 'c3';
+import * as d3 from 'd3';
 
 @Component({
     selector: 'page-dashboard',
@@ -10,10 +11,20 @@ import * as c3 from 'c3';
 export class DashboardPage {
 
     @ViewChild('dashboardChart') dashboardChart: ElementRef;
+    @ViewChild('tooltipTemplate') tooltipTemplate: ElementRef;
+
+    private data = {
+        title: "",
+        thisMonth: 0,
+        lastMonth: 0,
+        budgetLimit: 0,
+        trend: 0
+    };
 
     constructor(public navCtrl: NavController, public navParams: NavParams, private db: Database) { }
 
     ionViewDidLoad() {
+        let dashboard = this;
         let dashboardChartArea = this.dashboardChart.nativeElement;
         let data = this.db.getAllData().then((val) => {
             console.log(val);
@@ -25,10 +36,23 @@ export class DashboardPage {
                 },
                 donut: {
                     title: "Users age"
+                },
+                padding: {
+                    top: 0,
+                    right: 200,
+                    bottom: 40,
+                    left: 200,
+                },
+                tooltip: {
+                    contents: function(d, defaultTitleFormat, defaultValueFormat, color) {
+                        dashboard.data.title = d[0].name;
+                        dashboard.data.thisMonth = d[0].value;
+                        // return this.getTooltipContent(d, defaultTitleFormat, defaultValueFormat, color);
+                        return dashboard.tooltipTemplate.nativeElement.innerHTML;
+                    }
                 }
             };
             c3.generate(chartConfiguration);
-          }
-        );
-      }
+        });
+    }
 }
