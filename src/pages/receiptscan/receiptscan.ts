d2bd06488ecf63cf1f4db49c739bba99f4340cf2
leaveyourcave <leaveyourcave@gmail.com>
2017-03-26 00:00:24 +0100

    Realtime text recognition status

--- src/pages/receiptscan/receiptscan.ts
+++ src/pages/receiptscan/receiptscan.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Camera } from 'ionic-native';
 import Tesseract from 'tesseract.js';
@@ -12,24 +12,42 @@ export class ReceiptScanPage {
     @ViewChild('scannedImg')
     private scannedImg: ElementRef;
 
-    constructor(public navCtrl: NavController, public navParams: NavParams) { }
+    private recognizedText: string;
+    private progress: any;
+    private result: any;
+
+    constructor(public navCtrl: NavController, public navParams: NavParams, public cd: ChangeDetectorRef) { }
 
     ionViewDidLoad() {
 
-        // Camera.getPicture({
-        //     destinationType: Camera.DestinationType.FILE_URI,
-        //     quality: 50,
-        //     targetWidth: 1000,
-        //     targetHeight: 1000
-        // }).then((image) => {
-        //     this.imidz = image;
-        // }, (err) => {
-        //     console.log(err);
-        // });
+        Camera.getPicture({
+            destinationType: Camera.DestinationType.FILE_URI,
+            quality: 50,
+            targetWidth: 1000,
+            targetHeight: 1000
+        }).then((image) => {
+            this.scannedImg.nativeElement.src = image;
+            this.recognizeText(image);
+        }, (err) => {
+            console.log(err);
+        });
 
-        Tesseract.recognize(this.scannedImg.nativeElement.src)
+        this.recognizeText(this.scannedImg.nativeElement.src);
+    }
+
+    recognizeText(image) {
+        Tesseract.recognize(image)
+            .progress((progress) => {
+                console.log('progress', progress);
+                this.progress = progress.status + " [" + Math.ceil(progress.progress*100) + "%]";
+                this.cd.detectChanges();
+            })
             .then((tesseractResult) => {
+                this.result = tesseractResult;
                 console.log(tesseractResult);
+                this.recognizedText = tesseractResult.text;
+                this.cd.detectChanges();
             });
+
     }
 }
