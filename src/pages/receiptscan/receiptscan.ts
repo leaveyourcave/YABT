37b2c39fe00f939403d4b0127d5eab4b7982eef5
leaveyourcave <leaveyourcave@gmail.com>
2017-03-29 20:29:17 +0200

    Tesseract.js integrated with Cordoba Camera

--- src/pages/receiptscan/receiptscan.ts
+++ src/pages/receiptscan/receiptscan.ts
@@ -19,35 +19,41 @@ export class ReceiptScanPage {
     constructor(public navCtrl: NavController, public navParams: NavParams, public cd: ChangeDetectorRef) { }
 
     ionViewDidLoad() {
-
         Camera.getPicture({
             destinationType: Camera.DestinationType.FILE_URI,
-            quality: 50,
-            targetWidth: 1000,
-            targetHeight: 1000
+            encodingType: Camera.EncodingType.PNG,
+            quality: 100,
+            targetWidth: 200,
+            targetHeight: 300
         }).then((image) => {
             this.scannedImg.nativeElement.src = image;
-            this.recognizeText(image);
+            this.recognizeText(this.scannedImg.nativeElement.src);
         }, (err) => {
             console.log(err);
+            this.recognizeText(this.scannedImg.nativeElement.src); // probably we're in a browser
         });
-
-        this.recognizeText(this.scannedImg.nativeElement.src);
     }
 
     recognizeText(image) {
         Tesseract.recognize(image)
             .progress((progress) => {
-                console.log('progress', progress);
-                this.progress = progress.status + " [" + Math.ceil(progress.progress*100) + "%]";
-                this.cd.detectChanges();
+                var progressStatus = progress.status + " [" + Math.ceil(progress.progress * 100) + "%]";
+                console.log(progressStatus);
+
+                this.progress = progressStatus;
+            })
+            .catch(err => {
+                console.log(err);
             })
             .then((tesseractResult) => {
-                this.result = tesseractResult;
                 console.log(tesseractResult);
+                // console.log(JSON.stringify(tesseractResult)); - circular json
+
+                this.result = tesseractResult;
                 this.recognizedText = tesseractResult.text;
+
+                // let's refresh everything
                 this.cd.detectChanges();
             });
-
     }
 }
